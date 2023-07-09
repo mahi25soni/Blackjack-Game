b352cee40ca3123b9438daa01738c67ded888e2f
Mahendra Soni <msdraj02@protonmail.com>
2023-07-09 20:38:30 +0530

    js started

--- index.js
+++ index.js
@@ -1,48 +1,14 @@
+console.log("s;dfsdkfj")
 
-let allButtons = Array.from(document.getElementsByTagName("button"))
-let theme = document.getElementById("theme-setter")
-let olderBoss = document.getElementById("past-element")
-let boss = document.getElementById("future-element")
-let firstElement = document.getElementById("first-element")
-let secondElement = document.getElementById("second-element")
+let yourScore = document.getElementById("yourScore").innerHTML
+let dealerScore = document.getElementById("dealerScore").innerHTML
 
 
-allButtons.forEach(element => {
-    element.addEventListener("click", async ()=> {
-        olderBoss.remove()
-
-        const random = Math.floor(Math.random() * 3);
-        firstElement.innerHTML = element.innerHTML
-        secondElement.innerHTML = allButtons[random].innerHTML
-
-        
-        theme.appendChild(boss)
-        boss.classList.remove("hidden")
-        boss.classList.add("flex")
-
-        await new Promise(resolve => setTimeout(resolve, 0));
-
-        checkWinner(firstElement.innerHTML, secondElement.innerHTML)
-    })
-})
-
-
-let kind = {
-    "Stone":1,
-    "Paper":2,
-    "Scissor":3
+function hitButtonFunction() {
+    let random_number = getRandomNumber()
+    console.log(random_number)
 }
-function checkWinner(your, pc){
-    const checking = kind[pc] - kind[your]
 
-    if((checking === 1) || (checking === -2)){
-        alert("PC won")
-    }
-    else if(checking === 0){
-        alert("no one won")
-    }
-    else {
-        alert("YOu won")
-    }
-
+function getRandomNumber() {
+    return (Math.floor(Math.random() * 13)) + 1
 }
